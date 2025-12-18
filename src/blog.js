70dbaf0ec049c3e44415466c0816dcfb820eab5c
websting <anttonio.herrera@gmail.com>
2025-12-17 19:08:03 -0500

    Reduced text in blog article

--- src/blog.js
+++ src/blog.js
@@ -17,28 +17,11 @@ if (blogContent && blogList) {
       author: "John Doe",
       image: "arco.jpg",
       content: `
-<p>
-Architecture tells the story of humanity’s evolution, capturing the spirit of
-entire eras through stone, steel, and design. Architectural photography
-challenges us to see buildings not merely as structures, but as living
-narratives shaped by time.
-</p><br/>
-<p>
-Old Havana, with its vibrant colors and intricate details, offers a rich
-tapestry for architectural exploration. It is a must-visit destination for
-photographers seeking to blend history with artistry.
-</p><br/>
-<p>
-Below are a few essential tips and must-have gear considerations for
-architectural photography:
-</p><br/>
 
-<ul>
-  <li>-Use a wide-angle lens to capture the grandeur of buildings.</li>
-  <li>-Pay close attention to lines and symmetry for balanced compositions.</li>
-  <li>-Shoot during the golden hour for warm, soft lighting.</li>
-  <li>-Experiment with different perspectives to highlight unique features.</li>
-</ul>
+Architecture tells the story of humanity’s evolution through time.
+
+Old Havana, with its vibrant colors and intricate details, offers a rich
+tapestry for architectural exploration.
 `
 
 },
